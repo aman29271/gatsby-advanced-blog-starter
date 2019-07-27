678f26fd98e6125748bed5dea1d33284272ed168
Aman <aman29271@gmail.com>
2019-07-27 16:35:00 +0530

    update public

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---src-templates-page-js": hot(preferDefault(require("/home/phoenix/Desktop/gatsby-bootcamp-project/src/templates/page.js"))),
   "component---src-templates-tags-js": hot(preferDefault(require("/home/phoenix/Desktop/gatsby-bootcamp-project/src/templates/tags.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/phoenix/Desktop/gatsby-bootcamp-project/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/phoenix/Desktop/gatsby-bootcamp-project/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/home/phoenix/Desktop/gatsby-bootcamp-project/src/pages/about.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/home/phoenix/Desktop/gatsby-bootcamp-project/src/pages/blog.js"))),
